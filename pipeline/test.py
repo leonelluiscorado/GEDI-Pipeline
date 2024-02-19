fc1d12ee9d0bf559fffbb785053b3bca965bcf0e
leonelluiscorado <leonelluiscorado@gmail.com>
2024-02-19 11:04:37 +0000

    Update gitignore to exclude anything test

--- pipeline/test.py
+++
@@ -1,5 +0,0 @@
-from gedi_downloader import GEDIDownloader
-
-downloader = GEDIDownloader()
-
-downloader.download_granule(url="https://data.ornldaac.earthdata.nasa.gov/protected/gedi/GEDI_L4A_AGB_Density_V2_1/data/GEDI04_A_2019222172335_O03740_03_T02786_02_002_02_V002.h5")
